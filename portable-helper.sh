e8b07b6f98b3163023beb2f67898d42347c620e4
Kimiblock Moe <kimiblock@icloud.com>
2025-02-22 23:46:16 +0800

    We don't need waiting anymore

--- portable-helper.sh
+++ portable-helper.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
-function waitForStart() {
-	echo 1 >~/startSignal
+#function waitForStart() {
+	#echo 1 >~/startSignal
 	#inotifywait \
 	#	-e modify \
 	#	--quiet \
 	#	~/startSignal
-}
+#}
 
-waitForStart
+#waitForStart
 
 $@
 
-rm ~/startSignal
+#rm ~/startSignal
