391ef74560f7aa749355a2c3dd0c7956a73b80da
Kimiblock Moe <kimiblock@icloud.com>
2025-07-04 15:38:09 +0800

    feat: set a background for shell

--- portable.sh
+++ portable.sh
@@ -333,6 +333,7 @@ function execApp() {
 	-p Description="Portable Sandbox for ${appID}" \
 	-p Documentation="https://github.com/Kraftland/portable" \
 	-p Slice="portable-${friendlyName}.slice" \
+	-p --background='42' \
 	-p ExitType=cgroup \
 	-p NotifyAccess=all \
 	-p TimeoutStartSec=infinity \
