de4fdb238addb136c12007637b66d1628f9243a0
Kimiblock Moe <kimiblock@icloud.com>
2026-01-27 19:49:14 +0800

    go: readConf: parse mprisName

--- lib/daemon/daemon.go
+++ lib/daemon/daemon.go
@@ -188,6 +188,14 @@ func readConf(readConfChan chan int) {
 		pecho("crit", "Unable to parse stateDirectory: " + stateDirectoryReadErr.Error())
 	}
 
+	mprisName, mprisNameReadErr := regexp.Compile("mprisName=.*")
+	if mprisNameReadErr == nil {
+		confOpts.mprisName = tryProcessConf(string(mprisName.Find(confReader)), "mprisName")
+		pecho("debug", "Determined mprisName: " + confOpts.mprisName)
+	} else {
+		pecho("crit", "Unable to parse mprisName: " + mprisNameReadErr.Error())
+	}
+
 	waylandOnly, waylandOnlyReadErr := regexp.Compile("waylandOnly=.*")
 	if waylandOnlyReadErr != nil {
 		pecho("crit", "Unable to parse waylandOnly: " + waylandOnlyReadErr.Error())
