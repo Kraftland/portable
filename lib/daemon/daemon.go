9628fd63d004f99b1b61a311e3bdd53ebede7292
Kimiblock Moe <kimiblock@icloud.com>
2026-01-27 16:05:17 +0800

    Implement configuration determine logic

--- lib/daemon/daemon.go
+++ lib/daemon/daemon.go
@@ -10,10 +10,16 @@ const (
 	version float32 = 0.1
 )
 
+type portableConfigOpts struct {
+	confPath	string
+	friendlyName	string
+	appID		string
+}
+
 var (
-	internalLoggingLevel int
-	appID string
-	runtimeDir string
+	internalLoggingLevel	int
+	runtimeDir		string
+	confOpts		portableConfigOpts
 )
 
 func pecho(level string, message string) {
@@ -36,7 +42,7 @@ func pecho(level string, message string) {
 	}
 }
 
-func getVariables() {
+func getVariables(varChan chan int) {
 	var externalLoggingLevel = os.Getenv("PORTABLE_LOGGING")
 	switch externalLoggingLevel {
 		case "debug":
@@ -64,18 +70,101 @@ func getVariables() {
 			pecho("crit", "XDG_RUNTIME_DIR is not a directory")
 		}
 	}
-	appID = os.Getenv("appID")
-	if len(appID) == 0 {
-		pecho("crit", "Application ID unknown")
+	varChan <- 1
+}
+
+func isPathSuitableForConf(path string) (result bool) {
+	confInfo, confReadErr := os.Stat(path)
+	if confReadErr != nil {
+		pecho("debug", "Unable to pick configuration at " + path + " for reason: " + confReadErr.Error())
+	} else {
+		if confInfo.IsDir() == true {
+			pecho("debug", "Unable to pick configuration at " + path + " for reason: " + "is a directory")
+		}
+		pecho("debug", "using configuration from " + path)
+		result = true
+		return
+	}
+	result = false
+	return
+}
+
+func determineConfPath() () {
+	currentWd, wdErr := os.Getwd()
+	var portableConfigRaw string = os.Getenv("_portableConfig")
+	var portableConfigLegacyRaw string = os.Getenv("_portalConfig")
+	if len(portableConfigLegacyRaw) > 0 {
+		pecho("warn", "Using legacy configuration variable!")
+		portableConfigRaw = portableConfigLegacyRaw
+	}
+	if len(portableConfigRaw) == 0 {
+		pecho("crit", "_portableConfig undefined")
+	}
+	if isPathSuitableForConf(portableConfigRaw) == true {
+		confOpts.confPath = portableConfigRaw
+	} else if
+	isPathSuitableForConf("/usr/lib/portable/info" + portableConfigRaw + "/config") == true {
+		confOpts.confPath = "/usr/lib/portable/info" + portableConfigRaw + "/config"
+	} else if wdErr == nil {
+		if isPathSuitableForConf(currentWd + portableConfigRaw) == true {
+			confOpts.confPath = currentWd + portableConfigRaw
+		}
+	} else if wdErr != nil {
+		pecho("crit", "Unable to get working directory: " + wdErr.Error())
+	} else {
+		pecho("crit", "Unable to determine configuration location")
+	}
+}
+
+func readConf() {
+	determineConfPath()
+
+}
+
+func stopMainAppCompat() {
+	stopMainExec := exec.Command("systemctl", "--user", "stop", "app-portable-" + confOpts.friendlyName + ".slice")
+	stopMainExec.Stderr = os.Stdout
+	stopMainExecErr := stopMainExec.Run()
+	if stopMainExecErr != nil {
+		pecho("debug", "Stop " + "app-portable-" + confOpts.friendlyName + ".slice" + " failed: " + stopMainExecErr.Error())
+	}
+}
+
+func stopMainApp() {
+	stopMainExec := exec.Command("systemctl", "--user", "stop", "app-portable-" + confOpts.appID + ".service")
+	stopMainExec.Stderr = os.Stdout
+	stopMainExecErr := stopMainExec.Run()
+	if stopMainExecErr != nil {
+		pecho("debug", "Stop " + "app-portable-" + confOpts.appID + ".service" + " failed: " + stopMainExecErr.Error())
+	}
+}
+
+func stopSlice() {
+	stopMainExec := exec.Command("systemctl", "--user", "stop", "portable-" + confOpts.friendlyName + ".slice")
+	stopMainExec.Stderr = os.Stdout
+	stopMainExecErr := stopMainExec.Run()
+	if stopMainExecErr != nil {
+		pecho("debug", "Stop " + "portable-" + confOpts.friendlyName + ".slice" + " failed: " + stopMainExecErr.Error())
+	}
+}
+
+func stopApp(operation string) {
+	go stopMainApp()
+	go stopMainAppCompat()
+	go stopSlice()
+	switch operation {
+		case "normal":
+			pecho("debug", "Cleaning leftovers...")
+		default:
+			pecho("crit", "Unknown operation for stopApp: " + operation)
 	}
 }
 
 func startApp() {
-	sdExec := exec.Command("xargs", "-0", "-a", runtimeDir + "/portable/" + appID + "/bwrapArgs", "systemd-run")
+	sdExec := exec.Command("xargs", "-0", "-a", runtimeDir + "/portable/" + confOpts.appID + "/bwrapArgs", "systemd-run")
 	sdExec.Stderr = os.Stderr
 	sdExec.Stdout = os.Stdout
 	sdExec.Stdin = os.Stdin
-	fmt.Println("Executing ", sdExec)
 	sdExecErr := sdExec.Run()
 	if sdExecErr != nil {
 		fmt.Println(sdExecErr)
@@ -85,6 +174,13 @@ func startApp() {
 
 func main() {
 	fmt.Println("Portable daemon", version, "starting")
-	getVariables()
+	readConf()
+	varChan := make(chan int)
+	go getVariables(varChan)
+	getVariablesReady := <- varChan
+	if getVariablesReady == 1 {
+		pecho("debug", "getVariables is ready")
+	}
 	startApp()
+	stopApp("normal")
 }
