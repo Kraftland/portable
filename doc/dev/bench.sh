8a68412ccb98155f14f1c4d28f060445569835ad
Kimiblock Moe <kimiblock@icloud.com>
2026-01-28 16:06:59 +0800

    bench: run more times

--- doc/dev/bench.sh
+++ doc/dev/bench.sh
@@ -2,7 +2,7 @@
 
 echo "Running hyperfine..."
 
-hyperfine --warmup 2 --shell=none 'env _portableConfig=./doc/dev/conf-perf PORTABLE_LOGGING=debug ./portable.sh'
+hyperfine --warmup 5 --runs=500 --shell=none 'env _portableConfig=./doc/dev/conf-perf PORTABLE_LOGGING=debug ./portable.sh'
 
 echo "Running ts..."
 
