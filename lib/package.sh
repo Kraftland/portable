e8b9f267327b3c623128d4222b2bf97c4ff9d727
Kimiblock Moe <kimiblock@icloud.com>
2026-01-30 17:26:36 +0800

    Implement busCheck in Go

--- lib/package.sh
+++ lib/package.sh
@@ -4,10 +4,10 @@ if [[ ${pkgdir} ]]; then
 	installPrefix="${pkgdir}"
 fi
 
-install -vDm755 portable.sh "${installPrefix}/usr/bin/portable"
 install -d "${installPrefix}/usr/lib/"
 cp -r "lib" "${installPrefix}/usr/lib/portable"
 install -t "${installPrefix}/usr/share/portable" -Dm755 "share"/*
 install -vDm755 portable-pools "${installPrefix}/usr/bin/portable-pools"
 install -vDm755 portable-packer "${installPrefix}/usr/bin/portable-packer"
 cp -r lib/modules-load.d "${installPrefix}/usr/lib"
+install -vDm755 lib/daemon/portable-daemon "${installPrefix}/usr/bin/portable"
