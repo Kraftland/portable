5977b4b7c9532e66eec712a26ddfad31de7a3a7a
Kimiblock Moe <kimiblock@icloud.com>
2026-02-27 14:35:40 +0800

    lookup xdg in readConf

--- lib/daemon/daemon.go
+++ lib/daemon/daemon.go
@@ -3171,7 +3171,6 @@ func waitChan(tgChan chan int8, chanName string) {
 }
 
 func main() {
-	lookUpXDG()
 	runtimeOpt.userExpose = make(chan map[string]string, 2048)
 	sigChan := make(chan os.Signal, 1)
 	go signalRecvWorker(sigChan)
