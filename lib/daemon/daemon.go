4044e36a2dd835051b5d05ced079971b170b5f46
Kimiblock Moe <kimiblock@icloud.com>
2026-03-09 10:37:27 +0800

    daemon: allow receiving broadcast from daemon AuxStart signal

--- lib/daemon/daemon.go
+++ lib/daemon/daemon.go
@@ -1145,6 +1145,7 @@ func calcDbusArg(argChan chan []string) {
 		"--broadcast=org.freedesktop.portal.*=@/org/freedesktop/portal/*",
 
 		"--call=top.kimiblock.portable." + confOpts.appID + "=top.kimiblock.Portable.Controller.Stop@/top/kimiblock/portable/daemon",
+		"--broadcast=top.kimiblock.portable." + confOpts.appID + "=top.kimiblock.Portable.Controller.AuxStart@/top/kimiblock/portable/daemon",
 
 	)
 
