696a824f08373a99b93e87547a6cd2d015f4c123
Kimiblock Moe <kimiblock@icloud.com>
2026-03-22 21:44:53 +0800

    Support setting default configs

---
+++ lib/daemon/defaultConf.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+func setDefaultConfOpts() Config {
+	var config Config
+	sessionType := os.Getenv("XDG_SESSION_TYPE")
+	execEnv := os.Getenv("launchTarget")
+	if len(execEnv) > 0 {
+		execSlice := strings.Split(execEnv, " ")
+		config.Exec.Target = execSlice[0]
+		config.Exec.Arguments = execSlice[1:]
+	}
+	switch sessionType {
+		case "wayland":
+			config.Privacy.X11 = false
+		default:
+			config.Privacy.X11 = true
+	}
+
+	config.Network.Enable = true
+	config.Processes.Track = true
+	config.Privacy.ClassicNotifications = true
+	config.Advanced.Qt5Compat = true
+	config.Advanced.FlatpakInfo = true
+	config.Advanced.KDEStatus = true
+
+	return config
+}
