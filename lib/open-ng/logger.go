942a5c55bc3881172c9acb1b5c3ac5ca287bba7d
Kimiblock Moe <kimiblock@icloud.com>
2026-04-05 15:24:52 +0800

    logger: add space

--- lib/open-ng/logger.go
+++ lib/open-ng/logger.go
@@ -6,6 +6,6 @@ import (
 )
 
 var (
-	logger	= log.New(os.Stdout, "[Portable OpenURI] [Log]", 0)
-	warn	= log.New(os.Stdout, "[Portable OpenURI] [Warn]", 0)
+	logger	= log.New(os.Stdout, "[Portable OpenURI] [Log] ", 0)
+	warn	= log.New(os.Stdout, "[Portable OpenURI] [Warn] ", 0)
 )
