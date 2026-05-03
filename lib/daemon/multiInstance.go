a9a016b0cf9b5056631135012f9b8b56657090a5
Kimiblock Moe <kimiblock@icloud.com>
2026-05-03 12:35:49 +0800

    mi: defer only on success

--- lib/daemon/multiInstance.go
+++ lib/daemon/multiInstance.go
@@ -155,8 +155,10 @@ func busAuxStartReq(conn *godbus.Conn, tray bool, args []string, config Config, 
 		restoreTerm, err := setCBreak(os.Stdin)
 		if err != nil {
 			pecho("warn", "Could not set standard input mode: " + err.Error())
+		} else {
+			defer restoreTerm()
 		}
-		defer restoreTerm()
+
 		conn, err := net.Dial("unix", inFile)
 		if err != nil {
 			pecho("warn", "Could not stream standard input: " + err.Error())
