6347f3fe097e2628d94b468b7c1f30ee323272ac
Kimiblock Moe <kimiblock@icloud.com>
2026-06-18 14:15:04 +0800

    seccomp: filter execve() calls

--- lib/helper/seccomp.go
+++ lib/helper/seccomp.go
@@ -1,12 +1,81 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 	"syscall"
 
 	"github.com/seccomp/libseccomp-golang"
 )
+
+func readArgFromMemory(pid int, addr uint64) (string, error) {
+	if addr == 0 {
+		return "", errors.New(
+			"Could not read argument: Null pointer passed",
+		)
+	}
+	path := filepath.Join("/proc", strconv.Itoa(pid), "mem")
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	_, err = file.Seek(int64(addr), io.SeekStart)
+	if err != nil {
+		return "", err
+	}
+	reader := bufio.NewReader(file)
+	bytes, err := reader.ReadBytes(0)
+	switch err {
+		case nil:
+		case io.EOF:
+		default:
+			return "", err
+	}
+	str := string(bytes)
+	return strings.TrimSuffix(str, "\x00"), nil
+}
+
+func handleExecveCalls(notif *seccomp.ScmpNotifReq, fd seccomp.ScmpFd) error {
+	var args []string
+	argsAddr := notif.Data.Args
+	for _, addr := range argsAddr {
+		arg, _ := readArgFromMemory(
+			int(notif.Pid),
+			addr,
+		)
+		args = append(args, arg)
+	}
+	if len(args) == 0 {
+		return errors.New("Could not read syscall arguments: empty")
+	}
+	switch filepath.Base(args[0]) {
+		case "bash":
+			debug.Println("PID", notif.Pid, "spawned a bash shell")
+		case "chrome-sandbox":
+			debug.Println("PID", notif.Pid, "spawned a chrome sandbox")
+		default:
+			debug.Println("Got execve() with arguments:", args)
+	}
+	// Do nothing now
+	var resp = seccomp.ScmpNotifResp{
+		ID:	notif.ID,
+		Error:	0,
+		Val:	0,
+		Flags:	seccomp.NotifRespFlagContinue,
+	}
+	err := seccomp.NotifRespond(
+		fd,
+		&resp,
+	)
+	return err
+}
 
 func superviseSeccompNotif(fd seccomp.ScmpFd) {
 	var wg sync.WaitGroup
@@ -21,10 +90,25 @@ func superviseSeccompNotif(fd seccomp.ScmpFd) {
 			return
 		}
 		wg.Go(func() {
+			switch notif.Data.Syscall {
+				case syscall.SYS_EXECVE:
+					err := handleExecveCalls(notif, fd)
+					if err != nil {
+						warn.Println(
+							"Could not handle",
+							"execve():",
+							err,
+						)
+					}
+					return
+				default:
+			}
+
 			callName, err := notif.Data.Syscall.GetName()
 			if err != nil {
 				callName = strconv.Itoa(int(notif.Data.Syscall))
 			}
+
 			warn.Println(
 				"System call triggered:", callName,
 				"from PID", notif.Pid,
@@ -80,6 +164,7 @@ func createSeccompFilter() (err error) {
 		syscall.SYS_CHROOT,
 		syscall.SYS_MOUNT,
 		syscall.SYS_BIND,
+		syscall.SYS_EXECVE,
 	}
 
 	for _, rule := range notifyRules {
