cfc5e7126cfff2b5c04631606bf677db61374202
Kimiblock Moe <kimiblock@icloud.com>
2026-06-18 11:42:23 +0800

    catch execve calls

--- lib/helper/seccomp.go
+++ lib/helper/seccomp.go
@@ -1,15 +1,58 @@
 package main
 
 import (
+	"sync"
 	"syscall"
 
 	"github.com/seccomp/libseccomp-golang"
 )
 
-func createSeccompFilter() error {
+func superviseSeccompNotif(fd seccomp.ScmpFd) {
+	var wg sync.WaitGroup
+	for {
+		notif, err := seccomp.NotifReceive(fd)
+		if err != nil {
+			warn.Println(
+				"Could not receive seccomp signal:",
+				err,
+			)
+			terminateNotify <- 1
+			return
+		}
+		wg.Go(func() {
+			debug.Println(
+				"Got syscall", notif.Data.Syscall,
+				"from PID", notif.Pid,
+				"using architecture", notif.Data.Arch.String(),
+				"calling", notif.Data.Args,
+			)
+
+			// Do nothing now
+			var resp = seccomp.ScmpNotifResp{
+				ID:	notif.ID,
+				Error:	0,
+				Val:	0,
+				Flags:	seccomp.NotifRespFlagContinue,
+			}
+
+			err := seccomp.NotifRespond(
+				fd,
+				&resp,
+			)
+			if err != nil {
+				warn.Println(
+					"Could not respond seccomp signal:",
+					err,
+				)
+			}
+		})
+	}
+}
+
+func createSeccompFilter() (err error) {
 	filter, err := seccomp.NewFilter(seccomp.ActAllow)
 	if err != nil {
-		return err
+		return
 	}
 	defer filter.Release()
 
@@ -18,17 +61,22 @@ func createSeccompFilter() error {
 		seccomp.ActNotify,
 	)
 	if err != nil {
-		return err
+		return
 	}
 
 	err = filter.Precompute()
 	if err != nil {
-		return err
+		return
 	}
-
 	err = filter.Load()
 	if err != nil {
-		return err
+		return
 	}
-	return nil
+	fd, err := filter.GetNotifFd()
+	if err != nil {
+		return
+	}
+	go superviseSeccompNotif(fd)
+
+	return
 }
