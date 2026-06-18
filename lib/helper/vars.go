fef76460e268a2e43102643fa0ee8131f431e584
Kimiblock Moe <kimiblock@icloud.com>
2026-06-18 15:18:04 +0800

    coloured output for the helper

--- lib/helper/vars.go
+++ lib/helper/vars.go
@@ -7,8 +7,8 @@ import (
 )
 
 var (
-	debug = log.New(os.Stdout, "[Portable] [Init] ", 0)
-	warn = log.New(os.Stderr, "[Portable] [Init] [Warning] ", 0)
+	debug = log.New(os.Stdout, "\033[0m" + "\033[38;2;125;241;118m" + "[Init]	" + "\033[0m", 0)
+	warn = log.New(os.Stderr, "\033[0m" + "\033[38;2;255;209;59m" + "[Init]	" + "\033[0m", 0)
 
 	// Origin -> Dest, must lock to prevent races
 	fileSubstitutionMap	= make(map[string]string)
