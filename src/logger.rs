cc812ac2d3889dcd762378913a556c9f971653c4
Kimiblock Moe <kimiblock@icloud.com>
2026-07-22 16:56:47 +0800

    logger: implement console restore

--- src/logger.rs
+++ src/logger.rs
@@ -11,10 +11,58 @@ struct LogMessage<T: std::fmt::Display> {
 }
 
 pub async fn logger<T> (
-	mut log_rx: tokio::sync::mpsc::Receiver<LogMessage<T>>
+	mut log_rx: tokio::sync::mpsc::Receiver<LogMessage<T>>,
+	stop_tx: tokio::sync::mpsc::Sender<crate::stop::StopFunc>,
 )
 	where T: std::fmt::Display
 {
+	let is_terminal = {
+		let thread = tokio::task::spawn_blocking(
+			|| get_termios(),
+		)
+		.await;
+
+		let thread = match thread {
+			Ok(v)	=> {v}
+			Err(e)	=> {
+				eprintln!("Could not spawn task: {e:#?}");
+				panic!("{e:#?}")
+			}
+		};
+
+		match thread {
+			Some(v)	=> {
+
+			let func = Box::new(move || {
+							use std::os::fd::AsFd;
+							match nix::sys::termios::tcsetattr(
+								std::io::stdin().as_fd(),
+								nix::sys::termios::SetArg::TCSANOW,
+								&v,
+							) {
+								Ok(_)	=> {}
+								Err(e)	=> {
+									eprintln!("Could not restore console state: {e:#?}")
+								}
+							}
+						});
+
+				stop_tx.send(
+					crate::stop::StopFunc {
+						layer: crate::stop::FunctionLayer::Pre,
+						function: func,
+					},
+				).await.expect("Could not request termination inhibitor");
+				true
+			}
+			None	=> {
+				eprintln!("Could not detect terminal status");
+				false
+			}
+		}
+	};
+
+
 	let msg = tokio::select! {
 		log_msg = log_rx.recv()	=> {
 			match log_msg {
@@ -27,14 +75,14 @@ pub async fn logger<T> (
 	};
 }
 
-fn is_terminal() -> (bool, Option<nix::sys::termios::Termios>) {
+fn get_termios() -> Option<nix::sys::termios::Termios> {
 	use std::os::fd::AsFd;
 	match nix::sys::termios::tcgetattr(std::io::stdin().as_fd()) {
 		Ok(v)	=> {
-			return (true, Some(v));
+			return Some(v);
 		}
 		Err(_)	=> {
-			return (false, None);
+			return None;
 		}
 	}
 }
