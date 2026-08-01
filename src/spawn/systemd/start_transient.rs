2cdd8cf4a032ca7c46b5ac89b5ad744fa469af80
Kimiblock Moe <kimiblock@icloud.com>
2026-08-01 14:18:39 +0800

    port ProtectProc

--- src/spawn/systemd/start_transient.rs
+++ src/spawn/systemd/start_transient.rs
@@ -299,6 +299,16 @@ async fn generate_properties(
 		)
 	);
 
+	vec.push(
+		(
+			String::from("ProtectProc"),
+			OwnedValue::from(Str::from("invisible")),
+			/*
+				https://github.com/systemd/systemd/blob/6a863b4dc31adc49fdfdd5deba32ed1b115adda3/src/core/namespace.h#L40
+			*/
+		)
+	);
+
 	/*
 		TimeoutStartSec was not ported, we have stable systemd notify impl
 		SecureBits was not ported. It seems to require value 32 (bit mask 1 << 5)
