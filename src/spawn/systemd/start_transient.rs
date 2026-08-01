bbe3d54f2bb7e4c0d2e471ed7b6ec48c1bf4f209
Kimiblock Moe <kimiblock@icloud.com>
2026-08-01 14:19:43 +0800

    port ProcSubset

--- src/spawn/systemd/start_transient.rs
+++ src/spawn/systemd/start_transient.rs
@@ -309,6 +309,13 @@ async fn generate_properties(
 		)
 	);
 
+	vec.push(
+		(
+			String::from("ProcSubset"),
+			Str::from("pid").into(),
+		)
+	);
+
 	/*
 		TimeoutStartSec was not ported, we have stable systemd notify impl
 		SecureBits was not ported. It seems to require value 32 (bit mask 1 << 5)
