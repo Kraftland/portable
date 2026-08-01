033ccf594fd16c3cd4cfd38b022c18bfdf53da8b
Kimiblock Moe <kimiblock@icloud.com>
2026-08-01 12:01:19 +0800

    port KillMode over

--- src/spawn/systemd/start_transient.rs
+++ src/spawn/systemd/start_transient.rs
@@ -175,6 +175,13 @@ async fn generate_properties(
 		)
 	);
 
+	vec.push(
+		(
+			String::from("KillMode"),
+			OwnedValue::from(Str::from("control-group")),
+		)
+	);
+
 	/*
 		TimeoutStartSec was not ported, we have stable systemd notify impl
 		SecureBits was not ported. It seems to require value 32 (bit mask 1 << 5)
