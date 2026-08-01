c6a3acba22141f677fb7d72586dc653ea875bccc
Kimiblock Moe <kimiblock@icloud.com>
2026-08-01 12:12:36 +0800

    port SyslogIdentifier

--- src/spawn/systemd/start_transient.rs
+++ src/spawn/systemd/start_transient.rs
@@ -203,6 +203,13 @@ async fn generate_properties(
 		)
 	);
 
+	vec.push(
+		(
+			String::from("SyslogIdentifier"),
+			OwnedValue::from(Str::from(app_id)),
+		)
+	);
+
 	/*
 		TimeoutStartSec was not ported, we have stable systemd notify impl
 		SecureBits was not ported. It seems to require value 32 (bit mask 1 << 5)
