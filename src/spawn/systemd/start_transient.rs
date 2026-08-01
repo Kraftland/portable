36003c8eed4e6cae897152edfb0ba5dc85afe84d
Kimiblock Moe <kimiblock@icloud.com>
2026-08-01 14:08:56 +0800

    port RestrictRealtime

--- src/spawn/systemd/start_transient.rs
+++ src/spawn/systemd/start_transient.rs
@@ -292,6 +292,13 @@ async fn generate_properties(
 		)
 	);
 
+	vec.push(
+		(
+			String::from("RestrictRealtime"),
+			OwnedValue::from(true),
+		)
+	);
+
 	/*
 		TimeoutStartSec was not ported, we have stable systemd notify impl
 		SecureBits was not ported. It seems to require value 32 (bit mask 1 << 5)
