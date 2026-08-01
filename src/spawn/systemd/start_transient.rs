c414b1e28c75b8b2b1dda02ee5a84186c32c9466
Kimiblock Moe <kimiblock@icloud.com>
2026-08-01 13:59:22 +0800

    port CapabilityBoundingSet over

--- src/spawn/systemd/start_transient.rs
+++ src/spawn/systemd/start_transient.rs
@@ -268,6 +268,16 @@ async fn generate_properties(
 		)
 	);
 
+	vec.push(
+		(
+			String::from("CapabilityBoundingSet"),
+			/*
+				Bitmask here
+			*/
+			OwnedValue::from(0 as u64),
+		)
+	);
+
 	/*
 		TimeoutStartSec was not ported, we have stable systemd notify impl
 		SecureBits was not ported. It seems to require value 32 (bit mask 1 << 5)
