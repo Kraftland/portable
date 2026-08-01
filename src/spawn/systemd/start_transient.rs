a554017a6312fcaaa726cb67be39326ea713ddd5
Kimiblock Moe <kimiblock@icloud.com>
2026-08-01 12:05:22 +0800

    port IPAccounting over

--- src/spawn/systemd/start_transient.rs
+++ src/spawn/systemd/start_transient.rs
@@ -182,6 +182,13 @@ async fn generate_properties(
 		)
 	);
 
+	vec.push(
+		(
+			String::from("IPAccounting"),
+			OwnedValue::from(true),
+		)
+	);
+
 	/*
 		TimeoutStartSec was not ported, we have stable systemd notify impl
 		SecureBits was not ported. It seems to require value 32 (bit mask 1 << 5)
