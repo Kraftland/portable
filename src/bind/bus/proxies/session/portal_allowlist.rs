8dcfb64833de66158a2f15846d5958633afbba82
Kimiblock Moe <kimiblock@icloud.com>
2026-08-03 15:36:46 +0800

    session: define allowed portals

---
+++ src/bind/bus/proxies/session/portal_allowlist.rs
@@ -0,0 +1,27 @@
+pub async fn get_allowed_portals() -> Vec<String> {
+	vec![
+		"Account".to_string(),
+		"Camera".into(),
+		"Clipboard".into(),
+		"Email".into(),
+		"FileChooser".into(),
+		"Location".into(),
+		"InputCapture".into(),
+		"MemoryMonitor".into(),
+		"NetworkMonitor".into(),
+		"Notification".into(),
+		"OpenURI".into(),
+		"PowerProfileMonitor".into(),
+		"Print".into(),
+		"ProxyResolver".into(),
+		"RemoteDesktop".into(),
+		"ScreenCast".into(),
+		"Screenshot".into(),
+		"Secret".into(),
+		"Settings".into(),
+		"Trash".into(),
+		"Usb".into(),
+		"Wallpaper".into(),
+		"GlobalShortcuts".into(),
+	]
+}
