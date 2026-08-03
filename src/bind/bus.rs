ded1b64d1766608e5190763fb8a2944f57a82543
Kimiblock Moe <kimiblock@icloud.com>
2026-08-03 13:23:18 +0800

    proxies: implement partial bus sandboxing

--- src/bind/bus.rs
+++ src/bind/bus.rs
@@ -1,4 +1,5 @@
 pub mod rules;
+pub mod proxies;
 
 /**
 	The public struct proxy is used to define rules and sandboxing layer for xdg-dbus-proxy
@@ -6,4 +7,33 @@ pub mod rules;
 pub struct Proxy {
 	sandbox:	crate::bind::types::BindRules,
 	bus_access:	Vec<rules::BusAccessLevel>,
+	bus_address:	String,
+	logger:		crate::logger::LogSender,
+	proxy_address:	String,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum BusProxyError {
+
+}
+
+/**
+	The shared trait StartProxy dictates a common start method for D-Bus proxies.
+*/
+pub trait StartProxy: Sized {
+	fn start(
+		proxy: Proxy,
+	) -> impl std::future::Future<Output = Result<(), Self::ProxyError>> + Send;
+	type ProxyError;
+
+	/// See struct Proxy for more info
+	/**
+		Note that proxy_path internally translates to:
+			unix:path=<proxy_path>/bus
+		To workaround an issue with bind-mounting non-existent files
+	*/
+	fn new(
+		logger:		crate::logger::LogSender,
+		proxy_path:	std::path::PathBuf,
+	) -> impl std::future::Future<Output = Result<Self, Self::ProxyError>> + Send;
 }
