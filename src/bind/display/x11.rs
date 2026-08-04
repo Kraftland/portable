fa09fbb32e4c973399e4e4817ae630db74a11d99
Kimiblock Moe <kimiblock@icloud.com>
2026-08-04 22:04:15 +0800

    x11: implement xauth detection

--- src/bind/display/x11.rs
+++ src/bind/display/x11.rs
@@ -3,6 +3,8 @@ pub enum DisplayBindError {}
 
 pub struct X11;
 
+pub mod xauth;
+
 impl super::BindDisplay for X11 {
 	async fn bind(self) -> Result<crate::bind::types::BindRules, Self::DisplayBindError> {
 		unimplemented!()
