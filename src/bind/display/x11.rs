68776e658bda559530a87a060a5ebc204c466fc2
Kimiblock Moe <kimiblock@icloud.com>
2026-08-05 13:55:32 +0800

    x11: spawn and gather xauth rules

--- src/bind/display/x11.rs
+++ src/bind/display/x11.rs
@@ -1,12 +1,39 @@
 #[derive(Debug, thiserror::Error)]
-pub enum DisplayBindError {}
+pub enum DisplayBindError {
+	#[error("Could not bind X11 display: {0:#?}")]
+	SpawnError(tokio::task::JoinError),
+}
 
-pub struct X11;
+pub struct X11 {
+	pub logger:		crate::logger::LogSender,
+	pub home:		std::path::PathBuf,
+	pub env:		crate::envs::holder::HoldChannel,
+}
 
 pub mod xauth;
 
 impl super::BindDisplay for X11 {
 	async fn bind(self) -> Result<crate::bind::types::BindRules, Self::DisplayBindError> {
+		let mut ret = vec![];
+
+		let xauth_spawn = tokio::spawn(xauth::bind(self.home, self.env));
+
+
+
+		match xauth_spawn.await.map_err(DisplayBindError::SpawnError)? {
+			Ok(v)	=> {
+				ret.extend(v);
+			}
+			Err(e)	=> {
+				let _ = self.logger.send(
+					crate::logger::LogMessage {
+						level: crate::logger::LogLevel::Warn,
+						message: format!("{e:#?}"),
+					},
+				).await;
+			}
+		};
+
 		unimplemented!()
 	}
 
