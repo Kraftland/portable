c631cc19ef13d670cf2b1a54d74889bd774ac243
Kimiblock Moe <kimiblock@icloud.com>
2026-08-07 13:26:09 +0800

    GPU: move binding into separate module

--- src/bind/subsystems/devices/gpu.rs
+++ src/bind/subsystems/devices/gpu.rs
@@ -4,6 +4,7 @@ use crate::bind::types::BindRule;
 
 pub mod nvidia;
 pub mod prime;
+mod bind;
 
 #[derive(Error, Debug)]
 pub enum GPUError {
@@ -56,12 +57,12 @@ pub async fn scan(
 
 	if all_gpus {
 		for gpu in devices {
-			rules.extend(generate_bind_rules(gpu, &logger).await);
+			rules.extend(bind::generate_bind_rules(gpu, &logger).await);
 		};
 	} else {
 		for gpu in devices {
 			if gpu.boot_display {
-				rules.extend(generate_bind_rules(gpu, &logger).await);
+				rules.extend(bind::generate_bind_rules(gpu, &logger).await);
 			}
 		}
 	}
@@ -71,145 +72,6 @@ pub async fn scan(
 
 }
 
-async fn generate_bind_rules(
-	gpu:		GPUInfo,
-	logger:		&tokio::sync::mpsc::Sender<crate::logger::LogMessage>,
-) -> Vec<BindRule> {
-	let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
-
-	/*
-		sys/class/drm/ does not seem to be covered by udev
-	*/
-
-	{
-		let drm_path = std::path::PathBuf::from("/sys/class/drm");
-		match gpu.nodes.card_node {
-			Some(ref v)	=> {
-				let mut card_path = drm_path.clone();
-				card_path.push(v.sysname());
-				tx.send(
-					BindRule::Path {
-						source: card_path.clone(),
-						dest: card_path,
-						class: crate::bind::types::BindType::Device,
-					},
-				).expect("Error sending drm bind rules");
-			}
-			None	=> {
-				let _ = logger.send(
-					crate::logger::LogMessage {
-						level: crate::logger::LogLevel::Warn,
-						message: format!("Missing card node: {:#?}", gpu),
-					},
-				).await;
-			}
-		};
-		match gpu.nodes.render_node {
-			Some(ref v)	=> {
-				let mut card_path = drm_path.clone();
-				card_path.push(v.sysname());
-				tx.send(
-					BindRule::Path {
-						source: card_path.clone(),
-						dest: card_path,
-						class: crate::bind::types::BindType::Device,
-					},
-				).expect("Error sending drm bind rules");
-			}
-			None	=> {
-				let _ = logger.send(
-					crate::logger::LogMessage {
-						level: crate::logger::LogLevel::Warn,
-						message: format!("Missing renderer node: {:#?}", gpu),
-					},
-				).await;
-			}
-		}
-	};
-
-	match gpu.vendor {
-		GPUVendor::AMD			=> {
-			tx.send(
-				BindRule::Path {
-					source: std::path::PathBuf::from("/dev/kfd"),
-					dest: std::path::PathBuf::from("/dev/kfd"),
-					class: crate::bind::types::BindType::Device,
-				},
-			).unwrap();
-		}
-
-		GPUVendor::Intel		=> {
-			// Intel doesn't need any new tricks, yet
-		}
-		GPUVendor::NVIDIA {driver}	=> {
-			match driver {
-				nvidia::NVIDIADriver::Nouveau			=> {
-					// TODO: what about nouveau's modules?
-				}
-				nvidia::NVIDIADriver::Proprietary		=> {
-
-				}
-				nvidia::NVIDIADriver::Unknown { driver }	=> {
-					let _ = logger.send(
-						crate::logger::LogMessage {
-							level: crate::logger::LogLevel::Warn,
-							message: format!("Unknown nvidia driver: {driver:?}"),
-						},
-					).await;
-				}
-			}
-
-			let nv_modules_mount = tokio::task::spawn_blocking(|| {
-				nvidia_module_mounts(false)
-			});
-
-			let nv_modules_mount = match nv_modules_mount.await {
-				Ok(v)	=> {v}
-				Err(e)	=> {
-					let _ = logger.send(
-						crate::logger::LogMessage {
-							level: crate::logger::LogLevel::Warn,
-							message: format!(
-								"Could not apply NVIDIA quirks: {:#?}",
-								e,
-							),
-						}
-					).await;
-					return vec![];
-				}
-			};
-			for rule in nv_modules_mount {
-				tx.send(rule).unwrap();
-			};
-		}
-
-		GPUVendor::Others	=> {
-			let _ = logger.send(
-				crate::logger::LogMessage {
-					level: crate::logger::LogLevel::Warn,
-					message: format!("Could not apply GPU quirks: unknown vendor"),
-				}
-			).await;
-		}
-	};
-
-	rx.close();
-
-	let mut ret = vec![];
-
-	loop {
-		let msg = rx.recv().await;
-		match msg {
-			Some(v)	=> {
-				ret.push(v);
-			}
-			None	=> {
-				break;
-			}
-		}
-	}
-	ret
-}
 
 pub async fn gputest_print_all_devices(
 	tx: &tokio::sync::mpsc::Sender<crate::logger::LogMessage>
---
+++ src/bind/subsystems/devices/gpu/bind.rs
@@ -0,0 +1,144 @@
+/**
+	Generate a set of bind rules for a specific GPU
+*/
+pub async fn generate_bind_rules(
+	gpu:		super::GPUInfo,
+	logger:		&tokio::sync::mpsc::Sender<crate::logger::LogMessage>,
+) -> Vec<crate::bind::types::BindRule> {
+	use crate::bind::types::BindRule;
+	use super::GPUVendor;
+	let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+	/*
+		sys/class/drm/ does not seem to be covered by udev
+	*/
+
+	{
+		let drm_path = std::path::PathBuf::from("/sys/class/drm");
+		match gpu.nodes.card_node {
+			Some(ref v)	=> {
+				let mut card_path = drm_path.clone();
+				card_path.push(v.sysname());
+				tx.send(
+					BindRule::Path {
+						source: card_path.clone(),
+						dest: card_path,
+						class: crate::bind::types::BindType::Device,
+					},
+				).expect("Error sending drm bind rules");
+			}
+			None	=> {
+				let _ = logger.send(
+					crate::logger::LogMessage {
+						level: crate::logger::LogLevel::Warn,
+						message: format!("Missing card node: {:#?}", gpu),
+					},
+				).await;
+			}
+		};
+		match gpu.nodes.render_node {
+			Some(ref v)	=> {
+				let mut card_path = drm_path.clone();
+				card_path.push(v.sysname());
+				tx.send(
+					BindRule::Path {
+						source: card_path.clone(),
+						dest: card_path,
+						class: crate::bind::types::BindType::Device,
+					},
+				).expect("Error sending drm bind rules");
+			}
+			None	=> {
+				let _ = logger.send(
+					crate::logger::LogMessage {
+						level: crate::logger::LogLevel::Warn,
+						message: format!("Missing renderer node: {:#?}", gpu),
+					},
+				).await;
+			}
+		}
+	};
+
+	match gpu.vendor {
+		GPUVendor::AMD			=> {
+			tx.send(
+				BindRule::Path {
+					source: std::path::PathBuf::from("/dev/kfd"),
+					dest: std::path::PathBuf::from("/dev/kfd"),
+					class: crate::bind::types::BindType::Device,
+				},
+			).unwrap();
+		}
+
+		GPUVendor::Intel		=> {
+			// Intel doesn't need any new tricks, yet
+		}
+		GPUVendor::NVIDIA {driver}	=> {
+			match driver {
+				super::nvidia::NVIDIADriver::Nouveau			=> {
+					// TODO: what about nouveau's modules?
+				}
+				super::nvidia::NVIDIADriver::Proprietary		=> {
+
+				}
+				super::nvidia::NVIDIADriver::Unknown { driver }	=> {
+					let _ = logger.send(
+						crate::logger::LogMessage {
+							level: crate::logger::LogLevel::Warn,
+							message: format!("Unknown nvidia driver: {driver:?}"),
+						},
+					).await;
+				}
+			}
+
+			let nv_modules_mount = tokio::task::spawn_blocking(|| {
+				super::nvidia_module_mounts(false)
+			});
+
+			let nv_modules_mount = match nv_modules_mount.await {
+				Ok(v)	=> {v}
+				Err(e)	=> {
+					let _ = logger.send(
+						crate::logger::LogMessage {
+							level: crate::logger::LogLevel::Warn,
+							message: format!(
+								"Could not apply NVIDIA quirks: {:#?}",
+								e,
+							),
+						}
+					).await;
+					return vec![];
+				}
+			};
+			for rule in nv_modules_mount {
+				tx.send(rule).unwrap();
+			};
+		}
+
+		GPUVendor::Others	=> {
+			let _ = logger.send(
+				crate::logger::LogMessage {
+					level: crate::logger::LogLevel::Warn,
+					message: format!("Could not apply GPU quirks: unknown vendor"),
+				}
+			).await;
+		}
+	};
+
+	rx.close();
+
+	let mut ret = vec![];
+
+	loop {
+		let msg = rx.recv().await;
+		match msg {
+			Some(v)	=> {
+				ret.push(v);
+			}
+			None	=> {
+				break;
+			}
+		}
+	}
+	ret
+}
