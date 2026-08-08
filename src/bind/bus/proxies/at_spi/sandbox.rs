e8d45948ac27e3a29f56b9031a736fb18137c171
Kimiblock Moe <kimiblock@icloud.com>
2026-08-08 16:20:06 +0800

    implement sandbox for at-spi proxy

---
+++ src/bind/bus/proxies/at_spi/sandbox.rs
@@ -0,0 +1,54 @@
+pub async fn get_sandbox(
+	proxy_socket:	std::path::PathBuf,
+	host_socket:	std::path::PathBuf,
+	flatpak_info:	std::path::PathBuf,
+) -> Result<crate::bind::types::BindRules, super::AtspiError> {
+	use crate::bind::types::BindRule;
+
+	let ret = vec![
+		BindRule::Symlink {
+			source: "/usr/lib64".into(),
+			dest: "/lib64".into(),
+		},
+		BindRule::Path {
+			source: "/usr/lib".into(),
+			dest: "/usr/lib".into(),
+			class: crate::bind::types::BindType::ReadOnly,
+		},
+		BindRule::Path {
+			source: "/usr/lib64".into(),
+			dest: "/usr/lib64".into(),
+			class: crate::bind::types::BindType::ReadOnly,
+		},
+		BindRule::Path {
+			source: "/usr/bin".into(),
+			dest: "/usr/bin".into(),
+			class: crate::bind::types::BindType::ReadOnly,
+		},
+		BindRule::Path {
+			source: "/usr/bin".into(),
+			dest: "/usr/bin".into(),
+			class: crate::bind::types::BindType::ReadOnly,
+		},
+
+		BindRule::Path {
+			source: proxy_socket.clone(),
+			dest: proxy_socket,
+			class: crate::bind::types::BindType::ReadWrite,
+		},
+		BindRule::Path {
+			source: host_socket.clone(),
+			dest: host_socket,
+			class: crate::bind::types::BindType::ReadWrite,
+		},
+
+		#[cfg(feature = "flatpak")]
+		BindRule::Path {
+			source: flatpak_info,
+			dest: std::path::PathBuf::from("/.flatpak-info"),
+			class: crate::bind::types::BindType::ReadOnly,
+		}
+	];
+
+	Ok(ret)
+}
