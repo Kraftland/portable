4e7a6f0aada6c335456c18067f145e7d00aeaf94
Kimiblock Moe <kimiblock@icloud.com>
2026-08-10 13:06:09 +0800

    options: Introduce Actions

---
+++ src/pref/runtime/options.rs
@@ -0,0 +1,52 @@
+pub struct RuntimeOpts {
+	/**
+		File forwarding
+	*/
+	pub file_expose:	Option<std::collections::HashMap<std::path::PathBuf, std::path::PathBuf>>,
+}
+
+/**
+	StartMode designates different operation modes
+
+	Normal means continue starting or activating the instance
+
+	Action designates the previous --actions flag
+
+	When set to other values, the main thread aborts start up
+	and said actions should be handled in the cmdline module
+*/
+pub enum Action {
+	Normal,
+	Quit,
+}
+
+/**
+	This enum represents multiple possible ways for exposing a file
+
+	It is not the final representation of Files Map, as processing is needed to handle some files
+*/
+pub enum FileExposurePreference {
+	/**
+		Essentially bubblewrap's --bind flag, and BindRule::Path
+
+		Does not work on secondary instances since we can't really bind-mount after chroot
+
+		Cmdline rewrite is needed for this type
+	*/
+	MountPath {
+		host:	std::path::PathBuf,
+		dest:	std::path::PathBuf,
+		class:	crate::bind::types::BindType,
+	},
+
+	/**
+		Pass the file inside sandbox using XDG Desktop Portals
+
+		Works on secondary instances
+
+		Cmdline rewrite is needed for this type
+	*/
+	Passthrough {
+		host:	std::path::PathBuf,
+	},
+}
