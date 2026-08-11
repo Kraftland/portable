dfd6045ab0e7eaa338b617232cbe98343d6fb463
Kimiblock Moe <kimiblock@icloud.com>
2026-08-11 16:16:24 +0800

    user subsystem: bind home

---
+++ src/bind/subsystems/user/paths.rs
@@ -0,0 +1,33 @@
+
+pub async fn bind(
+	xdg_data_dir:	std::path::PathBuf,
+	xdg_home:	std::path::PathBuf,
+	state_dir:	String,
+) -> Result<crate::bind::types::BindRules, super::UserBindError> {
+	use crate::bind::types::BindRule;
+
+	let sandbox_home = {
+		let mut home = xdg_data_dir;
+		home.push(state_dir);
+		home
+	};
+
+	super::create_state_dir::create_state_dir(&sandbox_home).await?;
+
+	let ret = vec![
+		BindRule::Path {
+			source: sandbox_home.clone(),
+			dest: sandbox_home.clone(),
+			class: crate::bind::types::BindType::ReadWrite,
+		},
+		/*
+			We are using symlink here to avoid screwing up and exposing preferences
+		*/
+		BindRule::Symlink {
+			source: sandbox_home,
+			dest: xdg_home,
+		},
+	];
+
+	Ok(ret)
+}
