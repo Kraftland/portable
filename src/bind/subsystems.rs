328b1c2f0556b10204362811e4609327837b734d
Kimiblock Moe <kimiblock@icloud.com>
2026-08-15 21:53:36 +0800

    change the arrow

--- src/bind/subsystems.rs
+++ src/bind/subsystems.rs
@@ -224,7 +224,7 @@ pub async fn generate_bindrules(
 				key: "PS1".into(),
 				value: {
 					let mut ps1 = String::new();
-					ps1.push_str("⭃ 🗃 Portable: ");
+					ps1.push_str("╰─>Portable: ");
 					ps1.push_str(&config.metadata.sandbox_id);
 					ps1.push_str("·👻 ➵ ");
 					ps1
