ffd55aee8a1a44606c895b44846e518ffe208555
Kimiblock Moe <kimiblock@icloud.com>
2026-08-25 21:02:54 +0800

    tray: implement listings

---
+++ src/ipc/init/tray/list.rs
@@ -0,0 +1,85 @@
+/**
+	List registered StatusNotifierItems' bus name and their path
+*/
+pub async fn list(conn: &zbus::Connection, app_id: &str)
+-> zbus::fdo::Result<Vec<(String, String)>> {
+	let mut ret = vec![];
+
+	let proxy = StatusNotifierWatcherProxy::new(&conn)
+		.await
+		?;
+
+	let proxy_fdo = zbus::fdo::DBusProxy::new(&conn)
+		.await
+		?;
+
+	let names = proxy
+		.get_registered()
+		.await
+		?;
+
+	for name in names {
+		match name.split_once("@") {
+			Some((k, v))	=> {
+				let bus_name = match zbus::names::BusName::try_from(k) {
+					Ok(v)	=> v,
+					Err(e)	=> {
+						return Err(zbus::fdo::Error::InvalidArgs(format!("{e:#?}")));
+					}
+				};
+				let creds = proxy_fdo
+					.get_connection_credentials(bus_name)
+					.await
+					?;
+				let pid = match creds.process_fd() {
+					Some(v)	=> v,
+					None	=> {
+						return Err(
+							zbus::fdo::Error::InvalidArgs(
+								format!("No PID for {k}"),
+							),
+						);
+					}
+				};
+
+				let dir = {
+					let mut name = String::from("top.kimiblock.portable.");
+					name.push_str(&app_id);
+					let mut path = std::path::PathBuf::from("/proc");
+					path.push(&pid.to_string());
+					path.push("root");
+					path.push(&name);
+					path
+				};
+
+				if ! dir.exists() {
+					continue;
+				}
+
+
+				ret.push((k.to_string(), v.to_string()));
+			}
+			None		=> {
+				continue;
+			}
+		};
+	};
+
+	Ok(ret)
+}
+
+
+#[zbus::proxy(
+	default_service	= "org.kde.StatusNotifierWatcher",
+	default_path	= "/StatusNotifierWatcher",
+	interface	= "org.kde.StatusNotifierWatcher"
+)]
+trait StatusNotifierWatcher {
+	#[zbus(
+		name	= "RegisteredStatusNotifierItems",
+		property,
+	)]
+	fn get_registered(&self) -> zbus::Result<Vec<String>>;
+}
+
+
