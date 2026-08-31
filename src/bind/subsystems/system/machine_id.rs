6202f3cd0b63e94287f3d996f6b446a6e55f0f0c
Kimiblock Moe <kimiblock@icloud.com>
2026-08-31 13:19:45 +0800

    machine-id: implement higher level bind func

--- src/bind/subsystems/system/machine_id.rs
+++ src/bind/subsystems/system/machine_id.rs
@@ -1,4 +1,38 @@
+/**
+	Generates or retrieves the machine-id from a file
 
+	The default location to store which file is at XDG_CONFIG_HOME/portable/sandbox_id/machine-id
+*/
+pub async fn bind(
+	config:	std::sync::Arc<crate::config::Config>,
+	xdg:	std::sync::Arc<crate::xdg::XdgDirs>,
+)
+-> Result<crate::bind::types::BindRules, super::SystemBindError> {
+	let path = {
+		let mut path = xdg.config_home.to_path_buf();
+		path.push("portable");
+		path.push(&config.metadata.sandbox_id);
+		path.push("machine-id");
+		path
+	};
+
+	exist_or_generate_id(&path)
+		.await
+		?;
+
+	use crate::bind::types::BindRule;
+
+	Ok(
+		vec![
+			BindRule::Path {
+				source:	path,
+				dest:	"/etc/machine-id".into(),
+				class:	crate::bind::types::BindType::ReadOnly,
+			}
+		]
+	)
+
+}
 
 /**
 	This function accepts a give file path, and verifies that the file actually is present.
@@ -6,7 +40,7 @@
 	If it does not exist,
 		it is expected to generate machine-id then write to that specific file for future use.
 */
-async fn read_or_generate_id(path: std::path::PathBuf) -> Result<(), super::SystemBindError> {
+async fn exist_or_generate_id(path: &std::path::PathBuf) -> Result<(), super::SystemBindError> {
 	if tokio::fs::try_exists(&path).await.map_err(super::SystemBindError::IOError)? {
 		Ok(())
 	} else {
