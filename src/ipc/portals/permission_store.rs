53ccda331a90cf2195754d77c19a1e39f27093c9
Kimiblock Moe <kimiblock@icloud.com>
2026-09-14 15:18:12 +0800

    permission store: disable blocking APIs

--- src/ipc/portals/permission_store.rs
+++ src/ipc/portals/permission_store.rs
@@ -22,6 +22,8 @@ pub async fn list(bus: &zbus::Connection, table: &str) -> zbus::Result<Vec<Strin
 	interface	= "oorg.freedesktop.impl.portal.PermissionStore",
 	default_service	= "org.freedesktop.portal.Desktop",
 	default_path	= "/org/freedesktop/impl/portal/PermissionStore",
+	gen_async	= true,
+	gen_blocking	= false,
 )]
 trait PermissionStore {
 	async fn list(&self, table: &str) -> zbus::fdo::Result<Vec<String>>;
