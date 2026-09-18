ca025a4f2c583aa20d8ba1650c3269bba9f3534e
Kimiblock Moe <kimiblock@icloud.com>
2026-09-18 14:32:37 +0800

    user subsystem: implement permission matching

---
+++ src/bind/subsystems/user/expose/contain.rs
@@ -0,0 +1,43 @@
+
+/// True if expose_list is in the scope of rw, ro and device
+fn permission_contained(
+	rw:		&Vec<std::path::PathBuf>,
+	ro:		&Vec<std::path::PathBuf>,
+	device:		&Vec<std::path::PathBuf>,
+	expose_list:	&Vec<crate::pref::runtime::options::FileExposurePreference>,
+) -> bool {
+	for expose in expose_list {
+		match expose {
+			crate::pref::runtime::options::FileExposurePreference::MountPath { host, dest: _, class }
+			=> {
+				match class {
+					crate::bind::types::BindType::Device	=> {
+						if ! device.contains(host) {
+							return false;
+						}
+					}
+					crate::bind::types::BindType::ReadOnly	=> {
+						if ! ro.contains(host) {
+							return false;
+						}
+					}
+					crate::bind::types::BindType::ReadWrite	=> {
+						if ! rw.contains(host) {
+							return false;
+						}
+					}
+				}
+			}
+			crate::pref::runtime::options::FileExposurePreference::Passthrough { host }
+			=> {
+				if rw.contains(host) {
+					continue;
+				} else {
+					return false;
+				}
+			}
+		}
+	};
+
+	true
+}
